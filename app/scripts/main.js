b5616244bbd031921cfa3333333b4e5a4c3c9517
Shabith Ishan <shabith.ishan@gmail.com>
2014-09-16 21:17:55 +0530

    First Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+/*jshint newcap:false */
+/*global Raphael*/
+
+'use strict';
+
+$(function(){
+	var bg = Raphael(document.getElementById('fullWidthPaper'), '100%', '100%');
+	var paper = Raphael(document.getElementById('paper'), '100%', '100%');
+
+	//white triangle
+	bg.path('M')
+
+
+
+});
